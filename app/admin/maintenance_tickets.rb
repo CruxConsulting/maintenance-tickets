f075617b8a8ce7a0efb2ed27fc12a351706eb799
Bastien Rua <bastien@cruxandco.com>
2012-07-19 17:17:59 +0200

    setup the activeadmin maintenance ticket form

--- app/admin/maintenance_tickets.rb
+++ app/admin/maintenance_tickets.rb
@@ -1,3 +1,27 @@
+# encoding: UTF-8
+
 ActiveAdmin.register MaintenanceTicket do
-  
+
+  form do |f|
+    f.inputs do
+
+      f.input :maintained_by, as: :select, collection:
+      {
+        "Téléphone" => :phone,
+        "Prise de main" => :remote,
+        "Sur site" => "on-site"
+        }, include_blank: false
+
+      f.input :client_name
+
+      f.input :comment
+
+      f.input :state, as: :select, collection: {"Ouvert" => :open, "Fermé" => :closed}, include_blank: false
+
+    end
+
+    f.buttons
+
+  end
+
 end
