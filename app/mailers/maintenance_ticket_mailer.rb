8be4478f58a2fafd57f740fa80b3f320ef876f30
Bastien Rua <bastien@cruxandco.com>
2012-07-19 12:12:42 +0200

    implement the mailer send_ticket_info method

--- app/mailers/maintenance_ticket_mailer.rb
+++ app/mailers/maintenance_ticket_mailer.rb
@@ -1,3 +1,8 @@
 class MaintenanceTicketMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def send_ticket_infos(ticket)
+    mail(:to => "bastien@cruxandco.com", :subject => "the mail subject").inspect
+  end
+
 end
