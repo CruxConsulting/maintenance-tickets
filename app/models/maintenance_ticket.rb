385f7170b958050b73134213003df3a81590fe6e
Bastien Rua <bastien@cruxandco.com>
2012-07-19 12:13:07 +0200

    implements the after_create callback on MaintenanceTicket model

--- app/models/maintenance_ticket.rb
+++ app/models/maintenance_ticket.rb
@@ -1,3 +1,16 @@
 class MaintenanceTicket < ActiveRecord::Base
   attr_accessible :client_email, :client_name, :comment, :maintained_by, :state
+
+  # Callbacks
+  ###########
+
+  after_create :notify
+
+  # Instance methods
+  ##################
+
+  def notify
+    MaintenanceTicketMailer.send_ticket_infos(self).deliver
+  end
+
 end
