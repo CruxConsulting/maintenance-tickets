500f6ecc4d85dba8538cd068be3c91e202b405dc
Bastien Rua <bastien@cruxandco.com>
2012-07-20 16:21:21 +0200

    send the notification email to the client_email and environment variable

--- app/mailers/maintenance_ticket_mailer.rb
+++ app/mailers/maintenance_ticket_mailer.rb
@@ -1,9 +1,9 @@
 class MaintenanceTicketMailer < ActionMailer::Base
   default from: "support@asconseil.eu"
 
-  def send_ticket_infos(ticket)
+  def send_ticket_infos(ticket, recipients)
     @ticket = ticket
-    mail(:to => ENV["MAINTENANCE_TICKET_NOTIFICATION_EMAIL"], :subject => "notification: Nouveau ticket de maintenance")
+    mail(to: recipients, :subject => "notification: Nouveau ticket de maintenance")
   end
 
 end
