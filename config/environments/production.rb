48bc52bf8c2719ffb7820692aa7d87a8c999e9a1
Julien Palmas <julien.palmas@gmail.com>
2012-09-05 18:07:10 +0200

    configure dev, qa and prod for email default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ AdminAsconseilEu::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => "admin-asconseil-eu.herokuapp.com" }
+
 end
 
 ActionMailer::Base.smtp_settings = {
--- config/environments/qa.rb
+++ config/environments/qa.rb
@@ -65,6 +65,8 @@ AdminAsconseilEu::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.default_url_options = { :host => "asconseil-qa.herokuapp.com" }
+
 end
 
 ActionMailer::Base.smtp_settings = {
