2c53707c875cdd9f9c75ec52c518e1b9b221fbbc
Julien Palmas <julien.palmas@gmail.com>
2012-09-07 09:11:03 +0200

    add username attribute to admin_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120905142155) do
+ActiveRecord::Schema.define(:version => 20120907070542) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string    "resource_id",   :null => false
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20120905142155) do
     t.string    "last_sign_in_ip"
     t.timestamp "created_at",                             :null => false
     t.timestamp "updated_at",                             :null => false
+    t.string    "username"
   end
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
