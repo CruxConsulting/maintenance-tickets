f84c6258231be135a2069ca9461b217218bb2157
Julien Palmas <julien.palmas@gmail.com>
2013-02-12 18:08:14 +0100

    change email subject

--- app/mailers/maintenance_ticket_mailer.rb
+++ app/mailers/maintenance_ticket_mailer.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 class MaintenanceTicketMailer < ActionMailer::Base
   default from: "support@asconseil.eu"
 
@@ -13,12 +15,15 @@ class MaintenanceTicketMailer < ActionMailer::Base
   end
 
   def send_ticket_subject
-    subject = if ticket_created?
-      "notification: Nouveau ticket de maintenance"
+    status = if ticket_created?
+      "crée"
+    elsif @ticket.state == "Fermé"
+      "clôturé"
     else
-      "notification: Modification d'un ticket de maintenance"
+      "modifié"
     end
 
+    "Ticket numéro #{@ticket.id} #{status}"
   end
 
 end
