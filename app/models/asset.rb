91041bd91f99d72971ec33ac029c62fd96db3c0a
Julien Palmas <julien.palmas@gmail.com>
2015-03-04 23:19:01 +0100

    add expire_soon scope on assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,6 +1,16 @@
 class Asset < ActiveRecord::Base
+
   # Associations
   ##############
   belongs_to :client
+
+  # Class methods
+  ###############
+
+  # expire in 30 days or less
+  scope :expire_soon, -> {
+    t = arel_table
+    where(t[:expiration_date].lt Time.zone.today + 30)
+  }
 
 end
