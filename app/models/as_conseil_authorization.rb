81bbf0a6a671e02f4924a52aec677487db78940b
Julien Palmas <julien@crx.io>
2020-06-26 16:30:37 +0200

    better authorizations

--- app/models/as_conseil_authorization.rb
+++ app/models/as_conseil_authorization.rb
@@ -1,25 +1,19 @@
-class AssetMatcher
-  def ===(other)
-    ancestors = other.class === Class ? other.ancestors : other.class.ancestors
-    ancestors.include?(Asset)
-  end
-end
-
 class AsConseilAuthorization < ActiveAdmin::AuthorizationAdapter
 
   def authorized?(action, subject = nil)
-    return true if user.username == "crux"
+    return true if user.username == "cruxx"
     return true if (ENV["ADMINS"] || []).split(',').include? user.username
+    return true if action == :read
 
     case subject
-    when normalized(AdminUser)
-      false
+    when normalized(MaintenanceTicket)
+      return true
+    when normalized(MonthlyReport)
+      return true
     when normalized(PhoneAsset)
       (ENV["PHONE_ADMINS"] || []).split(',').include? user.username
-    when AssetMatcher.new
-      false
     else
-      true
+      ![:update, :create, :destroy].include?(action)
     end
   end
 
